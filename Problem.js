5c5f492613629cf9be5651d79778864a473151c9
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-07-27 22:28:34 +0600

    7 Problem sovled

--- Problem.js
+++ Problem.js
@@ -134,29 +134,3 @@ function romanToInt(roman) {
 
   return result;
 }
-
-// 8.  Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number.
-
-function findSecondSmallest(arr) {
-  if (arr.length < 2) {
-    return undefined;
-  }
-
-  let smallest = Infinity;
-  let secondSmallest = Infinity;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < smallest) {
-      secondSmallest = smallest;
-      smallest = arr[i];
-    } else if (arr[i] < secondSmallest && arr[i] !== smallest) {
-      secondSmallest = arr[i];
-    }
-  }
-
-  if (secondSmallest === Infinity) {
-    return undefined;
-  }
-
-  return secondSmallest;
-}
